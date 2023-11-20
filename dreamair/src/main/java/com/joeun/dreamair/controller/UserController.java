70f9e215e13f1c6bfeb2011db3d0128650c986ac
seulgi9256 <sgij56@gmail.com>
2023-11-20 18:15:31 +0900

    2023.11.20 비회원 주문내역 mapper.xml만 추가

--- dreamair/src/main/java/com/joeun/dreamair/controller/UserController.java
+++ dreamair/src/main/java/com/joeun/dreamair/controller/UserController.java
@@ -54,7 +54,7 @@ public class UserController {
      */
     // 회원권한(ROLE_USER)을 가진 사용자만 접근 허용
     // @PreAuthorize("hasRole('ROLE_USER')")
-    @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
+    // @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
     // @Secured("ROLE_USER")
     // @Secured({"ROLE_USER", "ROLE_ADMIN"})
     @GetMapping(value={"/", ""})
